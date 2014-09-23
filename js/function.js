514ce4b55fd76683080b6e4bccb2b65ca5b9cc49
luxiangrong <luxiangrongwave@gmail.com>
2014-09-23 17:15:56 +0800

    首页修改

--- js/function.js
+++ js/function.js
@@ -14,6 +14,18 @@
 				pagerCustom: '#bx-pager',
 				controls: false
 			});
+			
+			$(".search-tab li").on('click', function(){
+				$(".search-tab li").removeClass('active');
+				$(this).addClass('active');
+			});
+			
+			$(".cart-wrap").on('mouseenter', function() {
+				$(this).find('.cart-dialog').show();
+			});
+			$(".cart-wrap").on('mouseleave', function() {
+				$(this).find('.cart-dialog').hide();
+			});
 		});
 	});
 })(jQuery);
