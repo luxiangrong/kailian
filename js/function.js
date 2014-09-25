a0fb64c3dc63dd3710ed2a255040f644ab3eb456
luxiangrong <luxiangrongwave@gmail.com>
2014-09-25 08:15:13 +0800

    首页修改

--- js/function.js
+++ js/function.js
@@ -1,6 +1,7 @@
 (function($) {
 	$(function() {
 		$(document).ready(function() {
+			//推荐课程轮播效果
 			$("#owl-lessons").owlCarousel({
 				navigation : true, 
 				pagination: false,
@@ -10,21 +11,34 @@
 				responsive: false
 			});
 			
+			//首页轮播效果
 			$("#banner-slider").bxSlider({
 				pagerCustom: '#bx-pager',
 				controls: false
 			});
 			
+			//搜索tab的切换
 			$(".search-tab li").on('click', function(){
 				$(".search-tab li").removeClass('active');
 				$(this).addClass('active');
 			});
 			
+			//购物车详情的显示和隐藏
 			$(".cart-wrap").on('mouseenter', function() {
 				$(this).find('.cart-dialog').show();
 			});
 			$(".cart-wrap").on('mouseleave', function() {
 				$(this).find('.cart-dialog').hide();
+			});
+			
+			//瀑布流加载
+			$(".page-more a").on('click', function(e){
+				var __this = $(this);
+				e.preventDefault();
+				$.get('lessons-page.html',function(data){
+					console.log(data);
+					$(__this).closest('.page-more').before(data);
+				});
 			});
 		});
 	});
